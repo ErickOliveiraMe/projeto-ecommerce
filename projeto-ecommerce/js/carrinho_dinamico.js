6ad26af3f5c9af763ccb24b7301005eeaa3adedc
gustavog7dev <gerstmanngustavo@gmail.com>
2025-05-29 15:53:17 -0300

    Add files via upload

---
+++ projeto-ecommerce/js/carrinho_dinamico.js
@@ -0,0 +1,43 @@
+let counter = document.getElementById("counter_item_cart")
+let add = document.getElementById("add_one_cart_item") // adicionar mais 1
+let remove = document.getElementById("remove_one_cart_item") // remove mais 1
+let value_unique = document.getElementById("counter_value_unique") // Valor total
+let subtotal = document.getElementById("subtotal_cart") // carrinho inteiro 
+let frete = document.getElementById("frete")
+let discount = document.getElementById("discount") //carrinho inteiro
+let total = document.getElementById("total")
+
+
+
+
+//Remover 1 item do carrinho 
+function append_one_item(req){
+    if (req == 1){
+        let varia = Number(counter.getAttribute("valor"));
+        if(varia > 1) {
+            varia -= 1;
+            counter.setAttribute("valor", varia)
+            counter.innerHTML = varia;
+            value_total_item(varia)
+             
+        } 
+    }
+}
+
+//adicionar 1 item ao carrinho
+function add_one_item(req){
+    if (req == 1){
+        let varia = Number(counter.getAttribute("valor"));
+        varia += 1;
+        counter.setAttribute("valor", varia)
+        counter.innerHTML = varia;
+        value_total_item(varia)
+    }
+}
+
+function value_total_item(qtd_item){
+    let varia = Number(value_unique.getAttribute('valor'))
+    value_unique.innerHTML = `R$ ${qtd_item * varia}`
+    
+}
+
